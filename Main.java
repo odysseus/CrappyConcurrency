dd703b735d772404edfe43670d522b0bed69fa66
Ryan Case <rmcase@gmail.com>
2014-07-14 22:59:23 -0400

    Added in a number of locking examples

--- Main.java
+++ Main.java
@@ -8,5 +8,43 @@ public class Main {
                 System.out.println("Hello from new thread");
             }
         };
+        myThread.start();
+        Thread.yield();
+        System.out.println("Hello from main thread");
+        myThread.join();
+
+        class Counter {
+            private int count = 0;
+            public synchronized void increment() { ++count; }
+            public synchronized void inc() { ++count; }
+            public synchronized int getCount() { return count; }
+        }
+        final Counter counter = new Counter();
+
+        class CountingThread extends Thread {
+            public void run() {
+                for (int i=0; i<10000; i++) {
+                    counter.increment();
+                }
+            }
+        }
+
+        class CountThread extends Thread {
+            public void run() {
+                for (int i=0; i<10000; i++) {
+                    counter.inc();
+                }
+            }
+        }
+
+        CountingThread t1 = new CountingThread();
+        CountThread t2 = new CountThread();
+
+        t1.start(); t2.start();
+        t1.join(); t2.join();
+
+        System.out.println(counter.getCount());
+
+        Puzzle.exec();
     }
 }
